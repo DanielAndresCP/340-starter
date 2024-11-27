f602b61ec236f8f6ecfa13ebf2847c61ef380b83
Daniel Concha <danielconchaparra@gmail.com>
2024-11-27 11:26:52 -0300

    W04 - Assignment Task 2 - View Delivery

--- controllers/invController.js
+++ controllers/invController.js
@@ -50,6 +50,18 @@ invCont.buildInventoryManagement = async function (req, res, next) {
     });
 };
 
+// 8888888                           888b     d888                                     888
+//   888                             8888b   d8888                                     888
+//   888                             88888b.d88888                                     888
+//   888    88888b.   888  888       888Y88888P888  88888b.    .d88b.   88888b.d88b.   888888
+//   888    888 "88b  888  888       888 Y888P 888  888 "88b  d88P"88b  888 "888 "88b  888
+//   888    888  888  Y88  88P       888  Y8P  888  888  888  888  888  888  888  888  888
+//   888    888  888   Y8bd8P        888   "   888  888  888  Y88b 888  888  888  888  Y88b.
+// 8888888  888  888    Y88P         888       888  888  888   "Y88888  888  888  888   "Y888
+//                                                                 888
+//                                                            Y8b d88P
+//                                                             "Y88P"
+
 // Inventory management view
 /**
  *
@@ -62,6 +74,22 @@ invCont.buildInventoryManagement = async function (req, res, next) {
 
     res.render("./inventory/management", {
         title: "Inventory Management",
+        nav,
+    });
+};
+
+// Add classification view
+/**
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+invCont.buildAddClassification = async function (req, res, next) {
+    const nav = await utilities.getNav();
+
+    res.render("./inventory/add-classification", {
+        title: "Add Inventory Classification",
         nav,
     });
 };
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -6,6 +6,17 @@ const utilities = require("../utilities/index");
 const router = new express.Router();
 const invController = require("../controllers/invController");
 
+// 8888888                           8888888b.             888       888  d8b
+//   888                             888   Y88b            888       888  Y8P
+//   888                             888    888            888       888
+//   888    88888b.   888  888       888   d88P  888  888  88888b.   888  888   .d8888b
+//   888    888 "88b  888  888       8888888P"   888  888  888 "88b  888  888  d88P"
+//   888    888  888  Y88  88P       888         888  888  888  888  888  888  888
+//   888    888  888   Y8bd8P        888         Y88b 888  888 d88P  888  888  Y88b.
+// 8888888  888  888    Y88P         888          "Y88888  88888P"   888  888   "Y8888P
+//
+//
+//
 // Route to build inventory by classification view
 router.get(
     "/type/:classificationId",
@@ -18,7 +29,21 @@ router.get(
     utilities.handleErrors(invController.buildInventoryManagement)
 );
 
-// Invemtory management view
+// 8888888                           888b     d888                                     888
+//   888                             8888b   d8888                                     888
+//   888                             88888b.d88888                                     888
+//   888    88888b.   888  888       888Y88888P888  88888b.    .d88b.   88888b.d88b.   888888
+//   888    888 "88b  888  888       888 Y888P 888  888 "88b  d88P"88b  888 "888 "88b  888
+//   888    888  888  Y88  88P       888  Y8P  888  888  888  888  888  888  888  888  888
+//   888    888  888   Y8bd8P        888   "   888  888  888  Y88b 888  888  888  888  Y88b.
+// 8888888  888  888    Y88P         888       888  888  888   "Y88888  888  888  888   "Y888
+//                                                                 888
+//                                                            Y8b d88P
+//                                                             "Y88P"
 router.get("/", utilities.handleErrors(invController.buildInventoryManagement));
+router.get(
+    "/add-classification",
+    utilities.handleErrors(invController.buildAddClassification)
+);
 
 module.exports = router;
