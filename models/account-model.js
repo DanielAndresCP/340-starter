c9910891af11dbc4eca8f5faedd618d0fe3923d7
Daniel Concha <danielconchaparra@gmail.com>
2024-11-28 21:00:18 -0300

    W04 - Team Activity - Registration Validation and Form Stickiness

--- models/account-model.js
+++ models/account-model.js
@@ -26,4 +26,14 @@ async function registerAccount(
     }
 }
 
-module.exports = { registerAccount };
+async function checkExistingEmail(account_email) {
+    try {
+        const sql = "SELECT * FROM account WHERE account_email = $1";
+        const email = await pool.query(sql, [account_email]);
+        return email.rowCount !== 0;
+    } catch (error) {
+        return error.message;
+    }
+}
+
+module.exports = { registerAccount, checkExistingEmail };
--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,5 +1,6 @@
 const utilities = require("./index");
 const { body, validationResult } = require("express-validator");
+const accountModel = require("../models/account-model");
 const validate = {};
 
 /*  **********************************
@@ -30,7 +31,17 @@ validate.registrationRules = () => {
             .notEmpty()
             .isEmail()
             .normalizeEmail()
-            .withMessage("A valid email is required."),
+            .withMessage("A valid email is required.")
+            .custom(async (account_email) => {
+                const emailExists = await accountModel.checkExistingEmail(
+                    account_email
+                );
+                if (emailExists) {
+                    throw new Error(
+                        "Email exists. Please log in or use different email"
+                    );
+                }
+            }),
 
         // password is required and must be strong password
         body("account_password")
@@ -50,14 +61,26 @@ validate.registrationRules = () => {
 /* ******************************
  * Check data and return errors or continue to registration
  * ***************************** */
+/**
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ * @returns
+ */
 validate.checkRegData = async (req, res, next) => {
     const { account_firstname, account_lastname, account_email } = req.body;
     let errors = validationResult(req);
 
-    if (!errors.isEmpty()) {        
+    if (!errors.isEmpty()) {
+        errors.array().filter((x) => x.msg !== "Invalid value").forEach((x) => {
+            req.flash("error", x.msg);
+        });
+    }
+
+    if (!errors.isEmpty()) {
         let nav = await utilities.getNav();
         res.render("account/registration", {
-            errors,
             title: "Registration",
             nav,
             account_firstname,
