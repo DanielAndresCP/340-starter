69699d65ed197919438b341b13bd833be30db9ed
Daniel Concha <danielconchaparra@gmail.com>
2024-12-05 17:51:19 -0300

    W05 - Team Activity - Inventory Model

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -145,6 +145,18 @@ async function updateInventory({
     }
 }
 
+// Delete a vehicle item
+async function deleteInventory(inv_id) {
+    try {
+        const sql = `DELETE FROM inventory WHERE inv_id = $1;`;
+        const data = await pool.query(sql, [inv_id]);
+
+        return data;
+    } catch (error) {
+        console.error("Delete inventory error:", error);
+    }
+}
+
 module.exports = {
     getClassifications,
     getInventoryByClassificationId,
@@ -152,4 +164,5 @@ module.exports = {
     insertClassificationCategory,
     insertInventoryItem,
     updateInventory,
+    deleteInventory,
 };
